cf16c2e025a6cedf9fc3f8742b9febc03b6af043
sosnovskyas <sosnovsky.as@gmail.com>
2018-03-11 22:10:51 +0300

    implemented store

---
+++ src/modules/store.ts
@@ -0,0 +1,22 @@
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+// TODO: resolve webpack 4 import
+//noinspection TypeScriptUnresolvedFunction
+const thunkMiddleware = require('redux-thunk').default;
+
+import {combineReducers} from 'redux';
+import {goods} from './goods/goods-reducer';
+import {user} from './user/user-reducer';
+
+export const reducers = combineReducers({
+  goods,
+  user
+});
+
+export const configureStore = () => {
+    return createStore(reducers, composeWithDevTools(
+        applyMiddleware(thunkMiddleware)
+        // other store enhancers if any
+    ));
+};
